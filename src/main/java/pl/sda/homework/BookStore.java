67f4953fbf3cf092e7c21e7a89d18463942fd21b
Igor Sowinski <sowinski.igor@gmail.com>
2018-07-17 22:56:22 +0200

    My Change

--- src/main/java/pl/sda/homework/BookStore.java
+++ src/main/java/pl/sda/homework/BookStore.java
@@ -1,9 +1,6 @@
 package pl.sda.homework;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.HashMap;
+import java.util.*;
 
 public class BookStore {
 
@@ -12,14 +9,17 @@ public class BookStore {
     // wartość - ilość sztuk w magazynie
 
     public BookStore() {
+        Book book7 = new Book("Brent Weeks", "Nie znana", 1.0);
         Book book1 = new Book("Jakub Ćwiek", "Kłamca", 7.5);
         Book book2 = new Book("Jarosław Grzędowicz", "Pan Lodowego Ogrodu TOM 1", 8.3);
         Book book3 = new Book("Andrzej Sapkowski", "Pani Jeziorna", 1.0);
         Book book4 = new Book("Marcin Przybyłek", "Gamedec Zabaweczki", 7.1);
-        Book book5 = new Book("Brent Weeks", "Czarny Pryzmat", 9.9);
+        Book book5 = new Book("Brent Weeks", "Czarny Pryzmat", 9.1);
         Book book6 = new Book("Brent Weeks", "Droga Cienia", 9.8);
 
+
         bookCollection = new HashSet<>();
+        bookCollection.add(book7);
         bookCollection.add(book1);
         bookCollection.add(book2);
         bookCollection.add(book3);
@@ -29,12 +29,14 @@ public class BookStore {
 
 
         bookStorage = new HashMap<>();
+
         bookStorage.put(book1, 7);
         bookStorage.put(book2, 3);
         bookStorage.put(book3, 11);
         bookStorage.put(book4, 22);
         bookStorage.put(book5, 9);
         bookStorage.put(book6, 47);
+        bookStorage.put(book7, 2);
     }
 
     int collectionSize() { // metoda pomocna do junitów - nie potrzebna do działania programu
@@ -61,7 +63,8 @@ public class BookStore {
         System.out.println("Dodano nową książkę do kolekcji: " + book);
     }
 
-    public boolean findAuthorInCollection(String nameOfAuthor) {
+    public boolean findAuthorInCollection(String nameOfAuthor) { // metoda na potrzeby testow
+
 
         for (Book book : bookCollection) {
             if (book.getAuthor().equalsIgnoreCase(nameOfAuthor) == true) {
@@ -100,5 +103,101 @@ public class BookStore {
         System.out.println("Usunieto : " + amountOfRemovedBooks + " ksiazek " + nameOfAuthor);
     }
 
+    public List<Book> returnBooksSortedByTitle() {
 
+        List<Book> tempCollection = new ArrayList<>();
+
+        tempCollection.addAll(bookCollection);
+
+        tempCollection.sort(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+
+                return o1.getTitle().compareTo(o2.getTitle());
+            }
+        });
+        return tempCollection;
+    }
+
+    public List<Book> returnBooksSortedByAuthor(){
+
+        List<Book> tempCollection = new ArrayList<Book>();
+
+        tempCollection.addAll(bookCollection);
+
+
+        tempCollection.sort(new Comparator<Book>() {
+
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.getAuthor().compareTo(o2.getAuthor());
+            }
+        });
+        System.out.println(tempCollection);
+        return tempCollection;
+    }
+
+    public List<Book> returnBooksSortedByRating() {
+
+     List<Book> tempCollection = new ArrayList<Book>();
+
+     tempCollection.addAll(bookCollection);
+
+     tempCollection.sort(new Comparator<Book>() {
+         @Override
+         public int compare(Book book1, Book book2) {
+             return -Double.compare(book1.getRating(),book2.getRating());
+         }
+     });
+//        System.out.println(tempCollection);
+        return tempCollection;
+    }
+
+    public List<Book> returnAuthorsCollectionsSortedByOption(String nameOfAutor, List<Book> orderOfList){
+        List<Book> tempCollections = new ArrayList<Book>();
+        List<Book> ret = new ArrayList<Book>();
+
+        tempCollections.addAll(orderOfList);
+
+
+        for (int i = 0; i < tempCollections.size(); i++) {
+
+            if (tempCollections.get(i).getAuthor().equalsIgnoreCase(nameOfAutor)){
+
+                ret.add(tempCollections.get(i));
+            }
+        }
+        return ret;
+    }
+
+
+    /// STORAGE
+
+
+
+    public void reviewOfStorage(){
+
+        bookStorage.forEach((key, value) -> System.out.println(key + ":" + value));
+
+    }
+
+    public void addBookToStorage( ) {
+         int amountOfbooks = 10;
+        Book bookToAdd = new Book("Brent Weeks", "Czarny Pryzmat", 9.1);
+
+
+
+        if (bookToAdd.equals(bookStorage.containsKey(bookToAdd))) {
+
+            bookStorage.merge(bookToAdd,amountOfbooks,Integer::sum);
+            System.out.println("Zostala zwiekszona ilosc o : " + amountOfbooks);
+        }else {
+            bookStorage.put(bookToAdd,amountOfbooks);
+            System.out.println("Niestety nie bylo ksiazki na magazynie ale dodano nowa");
+        }
+
+
+
+
+    }
 }
